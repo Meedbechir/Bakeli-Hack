b98786099f09491a088cc83a6b2ebf8343ce648d
MeedBechir <medbechirathie@gmail.com>
2023-10-05 20:46:12 +0000

    another commit

--- src/components/DashCards.js
+++ src/components/DashCards.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const DashCards = ({ title, count }) => {
+export const DashCards = ({ title, count, cardColor }) => {
   return (
     <div className="card mb-5 mx-auto h-100" style={{ width: '22rem' }}>
       <div className="card-body pt-4">
--- src/components/Dashoard.js
+++ src/components/Dashoard.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { DashboardHeader } from './DashbordHeader';
 import { DashCards } from './DashCards'; 
 import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { FaMapMarker, FaClock, FaCalendar } from 'react-icons/fa'; // Importation des icônes de React Icons
 
 export const Dashboard = () => {
   const [numberOfTeachers, setNumberOfTeachers] = useState(0);
@@ -50,23 +51,57 @@ export const Dashboard = () => {
     getNumberOfStudents();
   }, []);
 
+  // Données des événements à venir
+  const upcomingEvents = [
+    {
+      title: 'Événement 1',
+      location: 'Salle A',
+      time: '10h00 - 12h00',
+      date: '2023-10-15',
+    },
+    {
+      title: 'Événement 2',
+      location: 'Salle B',
+      time: '14h00 - 16h00',
+      date: '2023-10-20',
+    },
+    {
+      title: 'Événement 3',
+      location: 'Salle C',
+      time: '09h30 - 11h30',
+      date: '2023-10-25',
+    },
+  ];
+
   return (
     <div>
       <DashboardHeader />
-    <div className="container-fluid mt-5 pt-5">
-      <div className="row my-3">
-        <div className="col-md-4 mb-3">
-          <DashCards title="Nombre de professeurs" count={numberOfTeachers} />
+      <div className="container-fluid mt-5 pt-5">
+        <div className="row my-3">
+          <div className="col-md-4 mb-3">
+            <DashCards title="Nombre de professeurs" count={numberOfTeachers}/>
+          </div>
+          <div className="col-md-4 mb-3">
+            <DashCards title="Nombre de cours" count={numberOfCourses} cardColor="course-card" />
+          </div>
+          <div className="col-md-4 mb-3">
+            <DashCards title="Nombre d'étudiants" count={numberOfStudents} cardColor="student-card" />
+          </div>
         </div>
-        <div className="col-md-4 mb-3">
-          <DashCards title="Nombre de cours" count={numberOfCourses} />
-        </div>
-        <div className="col-md-4 mb-3">
-          <DashCards title="Nombre d'étudiants" count={numberOfStudents} />
+        <div className="row mt-5 pt-3">
+          <h1>Les Événements à Venir</h1>
+          <ul className="event-list">
+            {upcomingEvents.map((event, index) => (
+              <li key={index} className="event-item">
+                <strong>{event.title}</strong><br />
+                <span className="event-info"><FaMapMarker size={32} /> Lieu : {event.location}</span><br />
+                <span className="event-info"><FaClock size={32} /> Heure : {event.time}</span><br />
+                <span className="event-info"><FaCalendar size={32} /> Date : {event.date}</span>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
-
-    </div>
     </div>
   );
 };
